6b5bf02e6a5f839caac291ee43604e31997f0d9d
NaddMija <lizarbieto1234@gmail.com>
2022-08-10 18:54:02 -0300

    se esta haciendo el header

--- FrontendLiz/src/app/app.module.ts
+++ FrontendLiz/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { SocialComponent } from './components/social/social.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAPComponent
+    LogoAPComponent,
+    SocialComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule
